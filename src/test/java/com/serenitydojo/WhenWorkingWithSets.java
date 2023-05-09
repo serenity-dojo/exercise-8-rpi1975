c10414acceb476d885101d5a2285d624a46e17b6
Reza Iskandar <rpi75@yahoo.com>
2023-05-08 20:19:28 -0500

    Completed exercise

--- src/test/java/com/serenitydojo/WhenWorkingWithSets.java
+++ src/test/java/com/serenitydojo/WhenWorkingWithSets.java
@@ -3,6 +3,7 @@ package com.serenitydojo;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,7 +18,12 @@ public class WhenWorkingWithSets {
     // We can create a set and add values to the set
     @Test
     public void creatingANewSet() {
-        Set<String> colors = null;
+        Set<String> colors = new HashSet<>(Arrays.asList("red","green", "blue"));
+        colors.add("red");
+        colors.add("green");
+        colors.add("blue");
+
+        // or do it like this: Set<String> colors = new HashSet<>(Arrays.asList("red","green", "blue"));
 
         // TODO: Create a new HashSet and add the values "red", "green", and "blue"
 
@@ -27,7 +33,7 @@ public class WhenWorkingWithSets {
     // We can create a set containing a specified list of values")
     @Test
     public void creatingASetOfValues() {
-        Set<String> colors = null;
+        Set<String> colors = Set.of("red","green","blue");
 
         // TODO: Create a set of Strings containing the values "red", "green" and "blue"
         //  using the Set.of() method
@@ -45,7 +51,7 @@ public class WhenWorkingWithSets {
         colors.add("yellow");
         colors.add("red");
 
-        int expectedSize = 0;
+        int expectedSize = colors.size();
 
         assertThat(colors.size()).isEqualTo(expectedSize);
     }
@@ -56,14 +62,14 @@ public class WhenWorkingWithSets {
         Set<String> colors = new HashSet<>();
 
         // TODO: Change this code so that it correctly determines whether the set is empty
-        Boolean startedEmpty = null;
+        Boolean startedEmpty = colors.isEmpty();
 
         colors.add("red");
         colors.add("green");
         colors.add("blue");
 
         // TODO: Change this code so that it correctly determines whether the set now empty or not
-        Boolean endedUpEmpty = null;
+        Boolean endedUpEmpty = colors.isEmpty();
 
         assertThat(startedEmpty).isTrue();
         assertThat(endedUpEmpty).isFalse();
